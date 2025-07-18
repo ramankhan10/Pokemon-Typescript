b225cd5f59b6f8fa7e1cd24b5a3c1c75469642da
Raman <khanahmadi.raman@gmail.com>
2025-07-18 16:29:43 +0300

    First push

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Pokemon } from "./pokemon";
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/pokemon.ts
@@ -0,0 +1,51 @@
+export type PokemonType = "Fire" | "Water" | "Grass" | "Electric";
+
+export class Pokemon {
+  private name!: string;
+  private type!: PokemonType;
+  private hp!: number;
+  private maxHp!: number;
+  private attackPower!: number;
+
+  constructor(
+    name: string,
+    type: PokemonType,
+    hp: number,
+    attackPower: number
+  ) {
+    this.name = name;
+    this.type = type;
+    this.hp = hp;
+    this.attackPower = attackPower;
+  }
+
+  //public Attak method
+
+  public attack(target: Pokemon): void {
+    console.log(`${this.name} to ${target.getName()} attack!`);
+    target.takeDamage(this.attackPower);
+  }
+
+  //method khesarat
+  public takeDamage(damage: number): void {
+    this.hp -= damage;
+    if (this.hp < 0) {
+      this.hp = 0;
+    }
+    console.log(`${this.name} ${damage} was injured and Now ${this.hp}`);
+  }
+
+  //method Getter baraye dastarsi be etlaat khososi
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getHp(): number {
+    return this.hp;
+  }
+
+  public isFainted(): boolean {
+    return this.hp <= 0;
+  }
+}
